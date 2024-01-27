09665d8d84c17974a12a7b9372fb38b0daf2a0e2
command-line <gregphiggins@gmail.com>
2024-01-27 19:05:21 +0000

    force release

--- src/main/java/com/fluxtion/maven/FluxtionScanToGenMojo.java
+++ src/main/java/com/fluxtion/maven/FluxtionScanToGenMojo.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author V12 Technology Ltd.
+ * @author greg higgins
  */
 @Mojo(name = "scan",
         requiresProject = true,
